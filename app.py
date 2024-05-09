9c60caf9a3991dd0430032b3ed84c68a35aa989e
Alfadli Rahmat Putra <121837863+AlfadliRPutra@users.noreply.github.com>
2024-05-09 16:08:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from text_processing import summarize_text
 
 def main():
     st.title("Peringkas Teks")
+    st.markdown("""<style>
+    .stTextInput>div:first-child {
+        background-color: #f0f0f0;
+        padding: 10px;
+    }
+    .stTextInput>div:first-child>textarea {
+        background-color: transparent !important;
+    }
+    .stButton>button {
+        background-color: #008080;
+        color: white;
+    }
+    </style>""", unsafe_allow_html=True)
     st.markdown("---")  # Garis pemisah
 
-    # Input teks pengguna dengan latar belakang berwarna
+    # Input teks pengguna
     input_text = st.text_area("Masukkan teks untuk diringkas", height=200, 
-                              max_chars=10000, background="#f0f0f0", 
+                              max_chars=10000, 
                               help="Anda dapat mengetik teks di sini")
 
-    # Tombol untuk memproses teks dengan latar belakang dan teks warna yang berbeda
+    # Tombol untuk memproses teks
     if st.button("Ringkaskan", 
                  help="Klik tombol ini untuk memproses teks dan melihat ringkasannya", 
                  key="summarize_button"):
