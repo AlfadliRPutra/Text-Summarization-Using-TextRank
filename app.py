0ba0ba5a58e54927259d8a67472b6ba29a7b63eb
Alfadli Rahmat Putra <121837863+AlfadliRPutra@users.noreply.github.com>
2025-09-05 21:15:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import streamlit as st
-import hydralit_components as hc
 from text_processing import summarize_text
 
 def main():
-    # Navbar
-    menu_data = [
-        {'icon': "fas fa-home", 'label': "Beranda"},
-        {'icon': "fas fa-file-alt", 'label': "Summary"},
-        {'icon': "fas fa-book", 'label': "Tentang"},
-        {'icon': "fas fa-envelope", 'label': "Kontak"}
-    ]
-    menu_id = hc.nav_bar(menu_definition=menu_data, sticky_nav=True, sticky_mode='pinned')
-    
+    # Sidebar menu
+    menu = st.sidebar.radio(
+        "Navigasi",
+        ["Beranda", "Summary", "Tentang", "Kontak"],
+        index=0
+    )
+
     st.markdown("""<style>
     .stTextInput>div:first-child {
         background-color: #f0f0f0;
@@ -25,9 +22,9 @@ def main():
     .stButton>button {
         background-color: #615EF0;
         color: white;
-        width: 150px;  /* Lebar tombol */
-        height: 50px;  /* Tinggi tombol */
-        font-size: 16px;  /* Ukuran font */
+        width: 150px;
+        height: 50px;
+        font-size: 16px;
         font-family: 'inter';
     }
     .welcome-text {
@@ -44,50 +41,47 @@ def main():
         color: #888888;
     }
     .content {
-        margin: 0 150px;  /* Tambahkan margin kiri dan kanan sebesar 50px */
+        margin: 0 150px;
     }
-    /* Adjust subheader size for summary */
     .summary-subheader {
         font-size: 24px;
     }
-    /* Adjust instruction text size for textarea */
     .textarea-instruction {
         font-size: 18px;
     }
     </style>""", unsafe_allow_html=True)
-    
+
     # Navigasi ke menu yang dipilih
-    if menu_id == "Beranda":
+    if menu == "Beranda":
         navigate_to_home()
-    elif menu_id == "Summary":
+    elif menu == "Summary":
         navigate_to_summary()
-    elif menu_id == "Tentang":
+    elif menu == "Tentang":
         navigate_to_about()
-    elif menu_id == "Kontak":
+    elif menu == "Kontak":
         navigate_to_contact()
 
 def navigate_to_home():
-    col1, col2 = st.columns([2, 1])  # Menentukan lebar kolom
+    col1, col2 = st.columns([2, 1])
     col1.markdown("<div class='welcome-text'>Selamat datang di AlphaSum</div>", unsafe_allow_html=True)
     col1.markdown("<div style='font-size: 24px;'>Aplikasi ini merupakan implementasi dari Natural Language Processing (NLP) yang memanfaatkan metode Text Summarization menggunakan algoritma TextRank. Dengan menggunakan teknologi ini, Anda dapat memasukkan teks panjang dan mendapatkan ringkasan singkat yang mewakili inti dari teks tersebut. Ringkasan ini dapat sangat berguna untuk memahami teks yang kompleks, mengekstrak informasi penting, atau membuat konten yang lebih mudah dipahami.</div>", unsafe_allow_html=True)
     with col2:
-        st.write("")  # Spasi kosong untuk memberi ruang antara teks dan tombol
-        st.markdown("<div class='centered-button'><button class='stButton'>Navigate to Summary</button></div>", unsafe_allow_html=True)
+        st.write("")
+        if st.button("Navigate to Summary"):
+            st.session_state["menu"] = "Summary"
     add_margin()
     add_copyright()
 
-
 def navigate_to_summary():
-    # Input teks pengguna
-    input_text = st.text_area("Masukkan teks untuk diringkas", height=250, 
-                              max_chars=10000, 
-                              help="Anda dapat mengetik teks di sini", 
-                              key="summary_input")
+    input_text = st.text_area(
+        "Masukkan teks untuk diringkas",
+        height=250,
+        max_chars=10000,
+        help="Anda dapat mengetik teks di sini",
+        key="summary_input"
+    )
 
-    # Tombol untuk memproses teks
-    if st.button("Kirim", 
-                 help="Klik tombol ini untuk memproses teks dan melihat ringkasannya", 
-                 key="summarize_button"):
+    if st.button("Kirim", help="Klik tombol ini untuk memproses teks dan melihat ringkasannya", key="summarize_button"):
         if input_text:
             summary = summarize_text(input_text)
             st.markdown("<div class='summary-subheader'>Ringkasan:</div>", unsafe_allow_html=True)
@@ -115,8 +109,6 @@ def add_margin():
 def add_copyright():
     st.markdown("<div class='copyright'>© 2024 Alfadli R. P</div>", unsafe_allow_html=True)
 
-
-
 if __name__ == "__main__":
-    st.set_page_config(layout='wide')  # Set layout ke 'wide' untuk full screen
+    st.set_page_config(layout='wide')
     main()
